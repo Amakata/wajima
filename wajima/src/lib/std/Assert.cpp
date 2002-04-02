0a64d491db104459e7f6272d1e5ea12dbb143aa5
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-04-02 15:33:12 +0000

    double����assertEquals������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@10 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/std/Assert.cpp
+++ wajima/src/lib/std/Assert.cpp
@@ -1,4 +1,5 @@
 #include <std/Assert.h>
+#include <cmath>
 
 namespace zefiro_std {
 	namespace Assert {
@@ -16,6 +17,15 @@ namespace zefiro_std {
 			                               long lineNumber ,
 										   std::string  fileName ){
 			throw NotEqualException( expected , actual , lineNumber , fileName );
+		}
+		void assertEquals(	double expected ,
+							double actual ,
+							double delta ,
+							long lineNumber ,
+							std::string  fileName ){
+			if( fabs( expected - actual ) > delta ){
+				assertNotEqualImplementation( assertion_traits<double>::toString(expected) , assertion_traits<double>::toString(actual) , lineNumber , fileName );
+			}
 		}
 	};
 };
