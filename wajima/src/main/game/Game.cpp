8beb23a9d29454484fb5ea7a7f387f07dea6c1a4
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-04-28 13:36:16 +0000

    getPlayerSize()������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@52 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/main/game/Game.cpp
+++ wajima/src/main/game/Game.cpp
@@ -1,12 +1,14 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/Game.cpp,v 1.3 2002/04/28 13:24:35 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/Game.cpp,v 1.4 2002/04/28 13:36:16 ama Exp $
  */
 
 #include "Game.h"
 
 namespace zefiro_game {
+	Game::Game():_playerSize(2){
+	}
 	int Game::getPlayerSize() const{
-		return 0;
+		return _playerSize;
 	}
 };
 
--- wajima/src/main/include/Game.h
+++ wajima/src/main/include/Game.h
@@ -1,3 +1,7 @@
+/**
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/Attic/Game.h,v 1.3 2002/04/28 13:36:11 ama Exp $
+ */
+
 #ifndef __GAME_H__
 #define __GAME_H__
 
@@ -5,7 +9,10 @@ namespace zefiro_game {
 	class Game 
 	{
 	public:
+		Game();
 		int getPlayerSize() const;
+	private:
+		int _playerSize;
 	};
 };
 
