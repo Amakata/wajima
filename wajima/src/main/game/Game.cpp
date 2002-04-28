7ade80f41126f2aa8ffa6be1920e07643001b479
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-04-28 14:52:29 +0000

    ����������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@55 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/main/game/Game.cpp
+++ wajima/src/main/game/Game.cpp
@@ -1,14 +1,27 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/Game.cpp,v 1.4 2002/04/28 13:36:16 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/Game.cpp,v 1.5 2002/04/28 14:52:29 ama Exp $
  */
 
 #include "Game.h"
 
 namespace zefiro_game {
-	Game::Game():_playerSize(2){
+	Game::Game():_playerSize(2),_currentPhaseNumber(0),_currentTurnNumber(0){
 	}
 	int Game::getPlayerSize() const{
 		return _playerSize;
+	}
+	int Game::getCurrentPhaseNumber() const{
+		return _currentPhaseNumber;
+	}
+	int Game::getCurrentTurnNumber() const{
+		return _currentTurnNumber;
+	}
+	void Game::iteratePhase() {
+		++_currentPhaseNumber;
+		if( _currentPhaseNumber  == getPlayerSize() ){
+			++_currentTurnNumber;
+			_currentPhaseNumber = 0;
+		}
 	}
 };
 
--- wajima/src/main/include/Game.h
+++ wajima/src/main/include/Game.h
@@ -1,18 +1,45 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/Attic/Game.h,v 1.3 2002/04/28 13:36:11 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/Attic/Game.h,v 1.4 2002/04/28 14:52:25 ama Exp $
  */
 
 #ifndef __GAME_H__
 #define __GAME_H__
 
 namespace zefiro_game {
+	/**
+	 * Gameはゲーム全体を進行し、プレーヤーに操作の機会を配分することを役目とする。
+	 */
 	class Game 
 	{
 	public:
+		/**
+		 *	コンストラクタ
+		 */
 		Game();
+		/**
+		 * ゲームをするプレーヤー数の取得を行う。
+		 * 通常、人間１、コンピュータ１の合計２となる。
+		 * \return プレーヤーの数
+		 */
 		int getPlayerSize() const;
+		/**
+		 * 現在のフェーズ数の取得を行う。
+		 * \return 現在のフェーズ数。通常、0からgetPlayerSize()-1の値を返す。
+		 */
+		int getCurrentPhaseNumber() const;
+		/**
+		 * 現在のターン数の取得を行う。
+		 * \return 現在のターン数
+		 */
+		int getCurrentTurnNumber() const;
+		/**
+		 * フェーズを1進める。ターンも自動的に更新される。
+		 */
+		void iteratePhase();
 	private:
-		int _playerSize;
+		int _playerSize;			///<	プレーヤ人数
+		int _currentPhaseNumber;	///<	現在のフェーズ番号
+		int _currentTurnNumber;		///<	現在のターン番号
 	};
 };
 
