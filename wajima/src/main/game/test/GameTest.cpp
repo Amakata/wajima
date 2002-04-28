2c43137d2705b7f477a2544175ff0d245968d3b4
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-04-28 14:52:15 +0000

    ������������������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@54 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/main/game/test/GameTest.cpp
+++ wajima/src/main/game/test/GameTest.cpp
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/test/Attic/GameTest.cpp,v 1.4 2002/04/28 13:24:31 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/test/Attic/GameTest.cpp,v 1.5 2002/04/28 14:52:11 ama Exp $
  */
 
 #include "test/GameTest.h"
@@ -15,5 +15,20 @@ namespace zefiro_gametest{
 	}
 	void GameTest::testGetPlayerSize(){
 		CPPUNIT_ASSERT_EQUAL( 2 , _game->getPlayerSize() );
+	}
+	void GameTest::testGetPhaseNumber(){
+		CPPUNIT_ASSERT_EQUAL( 0 , _game->getCurrentPhaseNumber() );
+	}
+	void GameTest::testGetTurnNumber(){
+		CPPUNIT_ASSERT_EQUAL( 0 , _game->getCurrentTurnNumber() );
+	}
+	void GameTest::testIteratePhase(){
+		for( int turn = 0 ; turn < 10 ; ++turn ){
+			CPPUNIT_ASSERT_EQUAL( turn , _game->getCurrentTurnNumber() );
+			for( int phase = 0 ; phase < _game->getPlayerSize() ; ++phase ){
+				CPPUNIT_ASSERT_EQUAL( phase , _game->getCurrentPhaseNumber() );
+				_game->iteratePhase();
+			}
+		}
 	}
 };
--- wajima/src/main/include/test/GameTest.h
+++ wajima/src/main/include/test/GameTest.h
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/test/Attic/GameTest.h,v 1.5 2002/04/28 13:24:24 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/test/Attic/GameTest.h,v 1.6 2002/04/28 14:52:15 ama Exp $
  */
 
 #ifndef __GAMETEST_H__
@@ -16,11 +16,17 @@ namespace zefiro_gametest{
 	class GameTest : public CppUnit::TestCase {
 		CPPUNIT_TEST_SUITE( GameTest );
 		CPPUNIT_TEST( testGetPlayerSize );
+		CPPUNIT_TEST( testGetPhaseNumber );
+		CPPUNIT_TEST( testGetTurnNumber );
+		CPPUNIT_TEST( testIteratePhase );
 		CPPUNIT_TEST_SUITE_END();
 	public:
 		void setUp();
 		void tearDown();
 		void testGetPlayerSize();
+		void testGetPhaseNumber();
+		void testGetTurnNumber();
+		void testIteratePhase();
 	protected:
 		zefiro_game::Game *_game;
 	};
