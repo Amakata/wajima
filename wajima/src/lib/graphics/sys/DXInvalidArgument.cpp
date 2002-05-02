a716679ddf8fd0af0eb7da1f9212695c58992cf7
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-05-02 01:32:24 +0000

    DXException��InvalidArgument��������������������DXInvalidArguemnt������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@70 0d65b87a-bfcd-0310-b916-82c895c6ade2

---
+++ wajima/src/lib/graphics/sys/DXInvalidArgument.cpp
@@ -0,0 +1,34 @@
+/**
+ * $Header : $
+ */
+
+#include "graphics/sys/DXInvalidArgument.h"
+
+namespace zefiro_graphics {
+	DXInvalidArgument::DXInvalidArgument( HRESULT hr , zefiro_std::SourceLine sourceLine , std::string additionalMessage )
+		:DXException(hr,sourceLine,additionalMessage){
+	}
+	DXInvalidArgument::DXInvalidArgument( HRESULT hr , long lineNumber , std::string fileName ,std::string additionalMessage )
+		: DXException( hr , lineNumber , fileName , additionalMessage ){
+	}
+	DXInvalidArgument::DXInvalidArgument( const DXInvalidArgument &other ):DXException(other){
+	}
+	DXInvalidArgument::~DXInvalidArgument() throw(){
+	}
+	DXInvalidArgument &DXInvalidArgument::operator =( const DXInvalidArgument &other ){
+		DXException::operator =(other);
+		return *this;
+	}
+	zefiro_std::Exception *DXInvalidArgument::clone() const{
+		return new DXInvalidArgument( *this );
+	}
+	bool DXInvalidArgument::isInstanceOf( const zefiro_std::Exception::Type &exceptionType ) const{
+	     return exceptionType == type()  ||
+         DXException::isInstanceOf( exceptionType ) ||
+		 InvalidArgument::isInstanceOf( exceptionType );
+	}
+	zefiro_std::Exception::Type DXInvalidArgument::type(){
+		return zefiro_std::Exception::Type( "zefiro_graphics::DXInvalidArgument" );
+	}
+
+};
