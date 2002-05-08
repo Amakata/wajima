3fbd62482f384a72fbfe78b8cc9d00c067c75e8a
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-05-08 15:16:37 +0000

    OneTimeInclude������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@76 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/main/include/Player.h
+++ wajima/src/main/include/Player.h
@@ -1,7 +1,9 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/Attic/Player.h,v 1.1 2002/04/28 17:33:27 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/include/Attic/Player.h,v 1.2 2002/05/08 15:16:37 ama Exp $
  */
 
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
 namespace zefiro_game {
 	/**
 	 *	Playerはプレーヤの情報を持ち、行動の振る舞いを決定する。
@@ -17,3 +19,4 @@ namespace zefiro_game {
 		bool isComputer() const;
 	};
 };
+#endif //__PLAYER_H__
