16cf3ce8e334aaf506d844fc15e8cd63c534f1e8
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-05-20 18:35:06 +0000

    �������������������������� ������������������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@97 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/WinMain.cpp
+++ wajima/src/lib/WinMain.cpp
@@ -1,15 +1,18 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/Attic/WinMain.cpp,v 1.2 2002/05/13 16:51:46 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/Attic/WinMain.cpp,v 1.3 2002/05/20 18:35:06 ama Exp $
  */
 
 #include <fstream>
 #include <windows.h>
 
 #include <graphics/sys/D3D8.h>
+#include <thread/Thread.h>
+#include <std/Logger.h>
 
 #include "WindowClass.h"
 #include "Window.h"
 #include "resource.h"
+
 
 #include <cppunit/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
@@ -67,34 +70,114 @@ void AdapterDeviceOutput(){
         ofs.close();
 }
 
+HWND g_hwnd;
+
+class TestRunnable : public zefiro_thread::Runnable
+{
+public:
+	virtual void run(){
+		zefiro_thread::Thread *thread = zefiro_thread::Thread::getCurrentThread();
+		std::ostringstream ostrstr;
+		ostrstr << thread << " " << thread->getThreadID();
+		HDC hdc = GetDC( g_hwnd );
+		for( int y=0 ; y<320 ; y+=16 ){
+			TextOut(hdc,10,y,ostrstr.str().c_str(),ostrstr.str().size());
+			zefiro_thread::Thread::yield();
+		}
+		ReleaseDC( g_hwnd , hdc );
+	}
+};
+
+
+void ThreadTest(){
+	zefiro_thread::Runnable *r = new TestRunnable();
+	zefiro_thread::Thread *thread = new zefiro_thread::Thread(r);
+	thread->setJoinable( false );
+	thread->start();
+}
+
+class Process
+{
+public:
+	Process( std::string cmdLine ):_cmdLine(cmdLine),_process(NULL),_processID(0){
+	}
+	virtual ~Process(){
+	}
+	void start(){
+		PROCESS_INFORMATION processInformation;
+		STARTUPINFO	startupInfo;
+		memset( &startupInfo , 0 , sizeof(startupInfo) );
+		startupInfo.cb = sizeof(startupInfo);
+		if( 0 == CreateProcess(NULL,const_cast<char*>(_cmdLine.c_str()),NULL,NULL,FALSE,0,NULL,NULL, &startupInfo , &processInformation ) ){
+			WIN32ASSERT( GetLastError() );
+		}
+		_process = processInformation.hProcess;
+		_processID = processInformation.dwProcessId;
+	}
+protected:
+	std::string _cmdLine;
+	HANDLE _process;
+	DWORD _processID;
+};
+
+
+std::ofstream *g_ostr;
+
+
 LRESULT CALLBACK WndProc( HWND hWnd ,
                                                   UINT message ,
                                                   WPARAM wParam ,
                                                   LPARAM lParam )
 {
-        switch( message )
-        {
-        case WM_COMMAND:
-                switch( LOWORD(wParam) )
-                {
-                case IDM_TEST:
-                        UnitTest();
-                        PostQuitMessage(0);
-                        break;
-                case IDM_ADAPTER_DEVICE_OUTPUT:
-                        AdapterDeviceOutput();
-                        PostQuitMessage(0);
-                        break;
-                case IDM_EXIT:
-                        PostQuitMessage(0);
-                        break;
-                }
-                break;
-        case WM_DESTROY:
-                PostQuitMessage(0);
-                break;
-        default:
-                return( DefWindowProc( hWnd , message , wParam , lParam ) );
-        }
-        return 0L;
+	Process *process;
+	switch( message )
+    {
+	case WM_CREATE:
+		g_hwnd = hWnd;
+		g_ostr = new std::ofstream("logger.txt");
+		zefiro_std::Logger::setOutputter( g_ostr );
+		break;
+	case WM_COMMAND:
+		switch( LOWORD(wParam) )
+		{
+		case IDM_TEST:
+			UnitTest();
+			process = new Process("c:\\windows\\notepad.exe output.txt");
+			process->start();
+			delete process;
+			break;
+		case IDM_ADAPTER_DEVICE_OUTPUT:
+			AdapterDeviceOutput();
+			process = new Process("c:\\windows\\notepad.exe AdapterDevice.txt");
+			process->start();
+			delete process;
+			break;
+		case IDM_THREAD_TEST:
+			ThreadTest();
+			Sleep(1000);
+			process = new Process("c:\\windows\\notepad.exe logger.txt");
+			process->start();
+			delete process;
+			break;
+		case IDM_EXIT:
+			PostMessage( hWnd , WM_CLOSE , 0 , 0 );
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		g_ostr->close();
+		zefiro_std::Logger::resetOutputter();
+		delete g_ostr;
+		PostQuitMessage(0);
+		break;
+	case WM_CLOSE:
+		g_ostr->close();
+		zefiro_std::Logger::resetOutputter();
+		delete g_ostr;
+		PostQuitMessage(0);
+		break;
+	default:
+		return( DefWindowProc( hWnd , message , wParam , lParam ) );
+	}
+	return 0L;
 }
