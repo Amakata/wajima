626cb62b562bb698e04ea4218ac9ba3a82212531
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-05-31 19:12:10 +0000

    ��������ID������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@131 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/include/lib/resource.h
+++ wajima/include/lib/resource.h
@@ -1,8 +1,9 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/include/lib/Attic/resource.h,v 1.2 2002/05/20 18:35:41 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/include/lib/Attic/resource.h,v 1.3 2002/05/31 19:12:10 ama Exp $
  */
 
 #define IDM_EXIT	100
 #define IDM_TEST	200
 #define IDM_ADAPTER_DEVICE_OUTPUT	300
 #define	IDM_THREAD_TEST	400
+#define IDM_THREAD_NOTIFY 500
