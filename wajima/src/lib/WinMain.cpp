224346a991c296989a4b1bc9c64cba26b3b15ad8
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-07-03 14:11:13 +0000

    WinProc������������ZefirolibtestApp����������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@146 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/WinMain.cpp
+++ wajima/src/lib/WinMain.cpp
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/Attic/WinMain.cpp,v 1.12 2002/07/02 16:58:27 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/Attic/WinMain.cpp,v 1.13 2002/07/03 14:11:13 ama Exp $
  */
 
 #include <fstream>
@@ -11,6 +11,7 @@
 #include "system/Thread.h"
 #include "system/SyncObject.h"
 #include "std/Logger.h"
+#include "std/sys/Win32Exception.h"
 
 #include "WindowClass.h"
 #include "Window.h"
@@ -51,36 +52,13 @@ int APIENTRY WinMain( HINSTANCE hInstance ,
         return( msg.wParam );
 }
 
-void UnitTest( std::string output ){
-        std::ofstream        ofs;
-        ofs.open( output.c_str() );
-        CppUnit::TextUi::TestRunner runner;        
-        runner.addTest( CppUnit::TestFactoryRegistry::getRegistry().makeTest() );
-        runner.setOutputter( new CppUnit::TextOutputter(&runner.result(),ofs ) );
-        runner.run( "" , true );
-        ofs.close();
-}
-
-void AdapterDeviceOutput( std::string output ){
-        std::ofstream        ofs;
-        ofs.open( output.c_str() );
-        zefiro_graphics::D3D8 *d3d8 = new zefiro_graphics::D3D8();
-        std::vector<zefiro_graphics::Adapter> adapters = d3d8->getAdapterVector();
-        for( std::vector<zefiro_graphics::Adapter>::iterator adapter = adapters.begin() ; adapter < adapters.end(); ++adapter ){
-                 ofs << adapter->toString() << std::endl;
-        }
-        delete d3d8;
-        ofs.close();
-}
-
-HWND g_hwnd;
 
 
 
 class TestRunnable : public zefiro_system::Runnable
 {
 public:
-	TestRunnable():syncObject_(new zefiro_system::SyncObject()){
+	TestRunnable( HWND hWnd ):syncObject_(new zefiro_system::SyncObject()),hWnd_(hWnd){
 	}
 	virtual ~TestRunnable(){
 		delete syncObject_;
@@ -92,22 +70,23 @@ public:
 		zefiro_system::Thread *thread = zefiro_system::Thread::getCurrentThread();
 		std::ostringstream ostrstr;
 		ostrstr << thread << " " << thread->getThreadID();
-		HDC hdc = GetDC( g_hwnd );
+		HDC hdc = GetDC( hWnd_ );
 		for( int y=0 ; y<16 ; y+=16 ){
 			TextOut(hdc,10,y,ostrstr.str().c_str(),ostrstr.str().size());
 			syncObject_->wait();
 		}
-		ReleaseDC( g_hwnd , hdc );
+		ReleaseDC( hWnd_ , hdc );
 	}
 protected:
 	zefiro_system::SyncObject *syncObject_;
+	HWND hWnd_;
 };
 
 class ThreadTest{
 public:
-	static void create(){
+	static void create( HWND hWnd ){
 		if( count__ == 0 && !active__ ){
-			r__ = new TestRunnable();
+			r__ = new TestRunnable( hWnd );
 			thread__ = new zefiro_system::Thread(r__,"TestThread");
 		}
 	}
@@ -141,62 +120,102 @@ zefiro_system::Thread* ThreadTest::thread__;
 int ThreadTest::count__ = 0;
 bool ThreadTest::active__ = false;
 
+class ZefirolibTestApp
+{
+public:
+	ZefirolibTestApp( HWND hWnd ):hWnd_(hWnd){
+		configRead("zefirolibtest.ini");
+		ostr_.open(config_["logfile"].c_str());
+		zefiro_std::Logger::setOutputter( &ostr_ );
+	}
+	virtual ~ZefirolibTestApp(){
+		ostr_.close();
+		zefiro_std::Logger::resetOutputter();
+	}
+	void testUnit(){
+        std::ofstream ofs;
+        ofs.open( config_["unittestfile"].c_str() );
+        CppUnit::TextUi::TestRunner runner;        
+        runner.addTest( CppUnit::TestFactoryRegistry::getRegistry().makeTest() );
+        runner.setOutputter( new CppUnit::TextOutputter(&runner.result(),ofs ) );
+        runner.run( "" , true );
+        ofs.close();
+		exec( config_["editor"] + " " + config_["unittestfile"]);
+	}
+	void testGetAdapterDeviceInfo(){
+        std::ofstream        ofs;
+        ofs.open( config_["devicefile"].c_str() );
+        zefiro_graphics::D3D8 *d3d8 = new zefiro_graphics::D3D8();
+        std::vector<zefiro_graphics::Adapter> adapters = d3d8->getAdapterVector();
+        for( std::vector<zefiro_graphics::Adapter>::iterator adapter = adapters.begin() ; adapter < adapters.end(); ++adapter ){
+                 ofs << adapter->toString() << std::endl;
+        }
+        delete d3d8;
+        ofs.close();
+		exec( config_["editor"] + " " + config_["devicefile"]);
+	}
+	void testThread(){
+		ThreadTest::create( hWnd_ );
+		ThreadTest::start();
+		exec(config_["editor"] + " " + config_["logfile"]);
+	}
+	void testThreadNotify(){
+		ThreadTest::notify();
+	}
+protected:
+	void configRead( std::string configFilePath ){
+		std::ifstream ifs;
+		std::string name;
+		std::string value;
 
+		ifs.open( configFilePath.c_str() );
+		while( !ifs.eof() ){
+			ifs >> name >> value;
+			config_.insert( std::pair<std::string,std::string>( name , value ) );
+		}
+		ifs.close();
+	}
+	void exec( std::string cmdLine ){
+		zefiro_system::Process *process = new zefiro_system::Process( cmdLine );
+		try{
+			process->start();
+		}catch( zefiro_std::Win32Exception &e ){
+			std::string message = std::string(e.what()) + "\n" + cmdLine;
+			MessageBox( hWnd_ , message.c_str() , "Error" , MB_OK );
+		}
+		delete process;
+	}
+	HWND hWnd_;
+	std::ofstream ostr_;
+	std::map< std::string , std::string > config_;
+};
 
 LRESULT CALLBACK WndProc( HWND hWnd ,
                                                   UINT message ,
                                                   WPARAM wParam ,
-                                                  LPARAM lParam )
+												  LPARAM lParam )
 {
-	static std::ofstream *g_ostr;
-	static std::map< std::string , std::string > sIni;
-	static std::string sEditorPath;
-
-	zefiro_system::Process *process;
-	std::ifstream ifs;
+	static ZefirolibTestApp *app;
 
 	switch( message )
     {
 	case WM_CREATE:
-		ifs.open("zefirolibtest.ini");
-		while( !ifs.eof() ){
-			std::string name;
-			std::string value;
-			ifs >> name >> value;
-			sIni.insert( std::pair<std::string,std::string>( name , value ) );
-		}
-		ifs.close();
-		g_hwnd = hWnd;
-		g_ostr = new std::ofstream( sIni["logfile"].c_str() );
-		zefiro_std::Logger::setOutputter( g_ostr );
-		// ‰Šú‰»ƒtƒ@ƒCƒ‹‚Ì“Ç‚Ýž‚Ý
+		app = new ZefirolibTestApp( hWnd );
 		break;
 	case WM_COMMAND:
 		switch( LOWORD(wParam) )
 		{
 		case IDM_TEST:
-			UnitTest(sIni["unittestfile"]);
-			process = new zefiro_system::Process( sIni["editor"] + " " + sIni["unittestfile"] );
-			process->start();
-			delete process;
+			app->testUnit();
 			break;
 		case IDM_ADAPTER_DEVICE_OUTPUT:
-			AdapterDeviceOutput(sIni["devicefile"]);
-			process = new zefiro_system::Process( sIni["editor"] + " " + sIni["devicefile"]);
-			process->start();
-			delete process;
+			app->testGetAdapterDeviceInfo();
 			break;
 		case IDM_THREAD_TEST:
-			ThreadTest::create();
-			ThreadTest::start();
-			Sleep(1000);
-			ThreadTest::notify();
-			process = new zefiro_system::Process( sIni["editor"] + " " + sIni["logfile"] );
-			process->start();
-			delete process;
+			app->testThread();
 			break;
 		case IDM_THREAD_NOTIFY:
-			ThreadTest::notify();
+			app->testThreadNotify();
 			break;
 		case IDM_EXIT:
 			PostMessage( hWnd , WM_CLOSE , 0 , 0 );
@@ -204,15 +223,11 @@ LRESULT CALLBACK WndProc( HWND hWnd ,
 		}
 		break;
 	case WM_DESTROY:
-		g_ostr->close();
-		zefiro_std::Logger::resetOutputter();
-		delete g_ostr;
+		delete app;
 		PostQuitMessage(0);
 		break;
 	case WM_CLOSE:
-		g_ostr->close();
-		zefiro_std::Logger::resetOutputter();
-		delete g_ostr;
+		delete app;
 		PostQuitMessage(0);
 		break;
 	default:
