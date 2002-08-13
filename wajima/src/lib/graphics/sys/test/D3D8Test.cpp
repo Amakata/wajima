a65652fe579e548e6ce29ff9eca8a01db9c758f3
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-08-13 06:57:47 +0000

    ������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@160 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/sys/test/D3D8Test.cpp
+++ wajima/src/lib/graphics/sys/test/D3D8Test.cpp
@@ -1,31 +1,57 @@
-/**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/sys/test/Attic/D3D8Test.cpp,v 1.7 2002/04/29 16:25:57 ama Exp $
- */
+//CUPPA:include=+
+#include "graphics/sys/D3D8.h"
+//CUPPA:include=-
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestAssert.h>
 
-#include "graphics/sys/test/D3D8Test.h"
+//CUPPA:namespace=+
+namespace zefiro_graphics {
+//CUPPA:namespace=-
 
-CPPUNIT_TEST_SUITE_REGISTRATION( zefiro_graphicstest::D3D8Test );
+class D3D8Test : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(D3D8Test);
+//CUPPA:suite=+
+  CPPUNIT_TEST(testAdapterCount);
+  CPPUNIT_TEST(testAdapterModeCount);
+  CPPUNIT_TEST(testIsAvailable);
+  CPPUNIT_TEST(testGetAdapterVector);
+//CUPPA:suite=-
+  CPPUNIT_TEST_SUITE_END();
+private:
+	D3D8 *d3d8_;
+  // your stuff...
 
-namespace zefiro_graphicstest{
-	void D3D8Test::setUp(){
-		_d3d8 = new zefiro_graphics::D3D8();
+public:
+
+  virtual void setUp() { d3d8_ = new zefiro_graphics::D3D8(); }
+  virtual void tearDown() { delete d3d8_; }
+
+//CUPPA:decl=+
+  void testAdapterCount() {
+	CPPUNIT_ASSERT( 0 < d3d8_->getAdapterCount() );
+  }
+  void testAdapterModeCount() {
+	for( int i=0 ; i<d3d8_->getAdapterCount() ; ++i ){
+		CPPUNIT_ASSERT( 0 < d3d8_->getAdapterModeCount( i ) );
 	}
-	void D3D8Test::tearDown(){
-		delete _d3d8;
-	}
-	void D3D8Test::testAdapterCount(){
-		CPPUNIT_ASSERT( 0 < _d3d8->getAdapterCount() );
-	}
-	void D3D8Test::testAdapterModeCount(){
-		for( int i=0 ; i<_d3d8->getAdapterCount() ; ++i ){
-			CPPUNIT_ASSERT( 0 < _d3d8->getAdapterModeCount( i ) );
-		}
-	}
-	void D3D8Test::testGetAdapterVector(){
-		std::vector<zefiro_graphics::Adapter> adapters = _d3d8->getAdapterVector();
+  }
+  void testIsAvailable() {
+		std::vector<zefiro_graphics::Adapter> adapters = d3d8_->getAdapterVector();
 		CPPUNIT_ASSERT( 0 < adapters.size() );
-	}
-	void D3D8Test::testIsAvailable(){
-		CPPUNIT_ASSERT_EQUAL( true , _d3d8->isAvailable() );
-	}
+  }
+  void testGetAdapterVector() {
+		CPPUNIT_ASSERT_EQUAL( true , d3d8_->isAvailable() );
+  }
+//CUPPA:decl=-
 };
+
+}
+
+namespace zefiro_graphics {
+//CUPPA:impl=+
+//CUPPA:impl=-
+
+CPPUNIT_TEST_SUITE_REGISTRATION(D3D8Test);
+CPPUNIT_TEST_SUITE_NAMED_REGISTRATION(D3D8Test,"zefiro_graphics");
+
+}
