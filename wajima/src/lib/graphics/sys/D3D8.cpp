8411a106a93aadba992e091911c98ce0d5d19c2f
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-08-25 12:07:45 +0000

    createDevice��������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@174 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/sys/D3D8.cpp
+++ wajima/src/lib/graphics/sys/D3D8.cpp
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/sys/Attic/D3D8.cpp,v 1.9 2002/05/23 15:59:48 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/sys/Attic/D3D8.cpp,v 1.10 2002/08/25 12:07:45 ama Exp $
  */
 
 #include "std/Assert.h"
@@ -7,25 +7,26 @@
 #include "graphics/sys/D3D8.h"
 #include "graphics/sys/DXAssert.h"
 
+
 namespace zefiro_graphics {
 	D3D8::D3D8(){
-		_pD3D = Direct3DCreate8( D3D_SDK_VERSION );
+		pD3D_ = Direct3DCreate8( D3D_SDK_VERSION );
 	}
 	D3D8::~D3D8(){
 		if( isAvailable() ){
-			_pD3D->Release();
+			pD3D_->Release();
 		}
 	}
 	int D3D8::getAdapterCount() const{
 		ZEFIRO_STD_ASSERT( isAvailable() );
-		return _pD3D->GetAdapterCount();
+		return pD3D_->GetAdapterCount();
 	}
 	int D3D8::getAdapterModeCount( const int adapter ) const{
 		ZEFIRO_STD_ASSERT( isAvailable() );
-		return _pD3D->GetAdapterModeCount( adapter );
+		return pD3D_->GetAdapterModeCount( adapter );
 	}
 	bool D3D8::isAvailable() const{
-		return _pD3D != NULL;
+		return pD3D_ != NULL;
 	}
 	std::vector<Adapter> D3D8::getAdapterVector() const{
 		ZEFIRO_STD_ASSERT( isAvailable() );
@@ -39,19 +40,52 @@ namespace zefiro_graphics {
 		}
 		return result;
 	}
+	D3DDevice* D3D8::createDevice( const int adapterid , Mode &mode , bool windowed , bool threaded , HWND hwnd ) const{
+
+
+		D3DPRESENT_PARAMETERS param;
+		memset( &param , 0 , sizeof(param) );	
+		param.BackBufferWidth = mode.getWidth();
+		param.BackBufferHeight = mode.getHeight();
+		param.BackBufferFormat = mode.getFormat();
+		param.BackBufferCount = 1;
+		param.SwapEffect = D3DSWAPEFFECT_DISCARD;
+		param.Windowed = windowed;
+		param.EnableAutoDepthStencil = TRUE;
+		param.AutoDepthStencilFormat = D3DFMT_D16;	//	
+		LPDIRECT3DDEVICE8 pD3DDevice;
+		DWORD behaviorFlags = NULL;
+		if( threaded ){
+			behaviorFlags = D3DCREATE_MULTITHREADED;
+		}
+
+		HRESULT hr;
+		hr = pD3D_->CreateDevice( adapterid , D3DDEVTYPE_HAL  , hwnd  ,D3DCREATE_HARDWARE_VERTEXPROCESSING | behaviorFlags ,&param , &pD3DDevice );
+		if( D3D_OK != hr  ){
+			hr = pD3D_->CreateDevice( adapterid , D3DDEVTYPE_HAL , hwnd , D3DCREATE_SOFTWARE_VERTEXPROCESSING | behaviorFlags, &param , &pD3DDevice );
+		}
+		if( D3D_OK != hr ){
+			hr = pD3D_->CreateDevice( adapterid , D3DDEVTYPE_REF , hwnd , D3DCREATE_HARDWARE_VERTEXPROCESSING | behaviorFlags , &param , &pD3DDevice );
+		}
+		if( D3D_OK != hr ){
+			hr = pD3D_->CreateDevice( adapterid , D3DDEVTYPE_REF , hwnd , D3DCREATE_SOFTWARE_VERTEXPROCESSING | behaviorFlags , &param , &pD3DDevice );
+		}
+		DXASSERT( hr );
+		return new D3DDevice(pD3DDevice);
+	}
 	Mode D3D8::getAdapterMode( const int adapterid , const int modeid ) const{
 		ZEFIRO_STD_ASSERT( isAvailable() );
 		
 		D3DDISPLAYMODE d3dDisplayMode ;
-		HRESULT hr = _pD3D->EnumAdapterModes( adapterid , modeid , &d3dDisplayMode );
+		HRESULT hr = pD3D_->EnumAdapterModes( adapterid , modeid , &d3dDisplayMode );
 		DXASSERT( hr );
-		return Mode( modeid , d3dDisplayMode.Width , d3dDisplayMode.Height , d3dDisplayMode.RefreshRate , d3dDisplayMode.Format );
+		return Mode(  d3dDisplayMode.Width , d3dDisplayMode.Height , d3dDisplayMode.RefreshRate , d3dDisplayMode.Format );
 	}
 	std::string D3D8::getAdapterIdentifier( const int adapterid ) const{
 		ZEFIRO_STD_ASSERT( isAvailable() );
 
 		D3DADAPTER_IDENTIFIER8	d3dAdapterIdentifier;
-		HRESULT hr = _pD3D->GetAdapterIdentifier( adapterid , D3DENUM_NO_WHQL_LEVEL , & d3dAdapterIdentifier );
+		HRESULT hr = pD3D_->GetAdapterIdentifier( adapterid , D3DENUM_NO_WHQL_LEVEL , & d3dAdapterIdentifier );
 		DXASSERT( hr );
 		return std::string( d3dAdapterIdentifier.Description );
 	}
