812aea6efc963819f4fd62d3918525b76df13cb7
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-09-18 14:44:57 +0000

    ������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@195 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/sys/D3DTexture.cpp
+++ wajima/src/lib/graphics/sys/D3DTexture.cpp
@@ -1,9 +1,16 @@
 #include "graphics/sys/D3DTexture.h"
 
 namespace zefiro_graphics {
-	D3DTexture::D3DTexture( LPDIRECT3DTEXTURE8 texture ):texture_(texture){
+	D3DTexture::D3DTexture( LPDIRECT3DTEXTURE8 texture , D3DXIMAGE_INFO &info):texture_(texture),width_(info.Width),height_(info.Height){
 	}
 	D3DTexture::~D3DTexture(){
 		texture_->Release();
+		texture_ = NULL;
+	}
+	int D3DTexture::getHeight()const{
+		return height_;
+	}
+	int D3DTexture::getWidth()const{
+		return width_;
 	}
 };
