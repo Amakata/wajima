b2524a9571a4f39a5fe3e88cbde214cf8158051e
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-09-18 14:39:27 +0000

    ��������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@186 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/include/lib/graphics/sys/D3DTexture.h
+++ wajima/include/lib/graphics/sys/D3DTexture.h
@@ -3,16 +3,21 @@
 
 #include <string>
 #include <d3d8.h>
+#include <d3dx8mesh.h>
 
 namespace zefiro_graphics {
 	class D3DDevice;
 	class D3DTexture{
 		friend D3DDevice;
 	public:
-		D3DTexture( LPDIRECT3DTEXTURE8 texture );
+		D3DTexture( LPDIRECT3DTEXTURE8 texture , D3DXIMAGE_INFO &info );
 		virtual ~D3DTexture();
+		int getWidth()const;
+		int getHeight()const;
 	protected:
 		LPDIRECT3DTEXTURE8 texture_;
+		int width_;
+		int height_;
 	};
 };
 
