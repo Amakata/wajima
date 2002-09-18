6235b1eddb994081e4e6991abe3f278a62c7e8b4
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-09-18 14:43:21 +0000

    ����������������WinMain������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@191 0d65b87a-bfcd-0310-b916-82c895c6ade2

---
+++ wajima/src/lib/ThreadTest.cpp
@@ -0,0 +1,55 @@
+#include "ThreadTest.h"
+
+#include <sstream>
+#include <windows.h>
+
+TestRunnable::TestRunnable( HWND hWnd ):syncObject_(new zefiro_system::SyncObject()),hWnd_(hWnd){
+}
+TestRunnable::~TestRunnable(){
+	delete syncObject_;
+}
+void TestRunnable::notify(){
+	syncObject_->notify();
+}
+void TestRunnable::run(){
+	zefiro_system::Thread *thread = zefiro_system::Thread::getCurrentThread();
+	std::ostringstream ostrstr;
+	ostrstr << thread << " " << thread->getThreadID();
+	HDC hdc = GetDC( hWnd_ );
+	for( int y=0 ; y<16 ; y+=16 ){
+		TextOut(hdc,10,y,ostrstr.str().c_str(),(int)ostrstr.str().size());
+		syncObject_->wait();
+	}
+	ReleaseDC( hWnd_ , hdc );
+}
+
+void ThreadTest::create( HWND hWnd ){
+	if( count__ == 0 && !active__ ){
+		r__ = new TestRunnable( hWnd );
+		thread__ = new zefiro_system::Thread(r__,"TestThread");
+	}
+}
+void ThreadTest::start(){
+	if( count__ == 0 && !active__ ){
+		thread__->setJoinable( false );
+		thread__->start();
+		active__ = true;
+	}
+}
+void ThreadTest::notify(){
+	if( !active__ ){
+		return;
+	}
+	if( count__ < 1 ){
+		r__->notify();
+		++count__;
+	}else{
+		count__ = 0;
+		active__ = false;
+	}
+}
+
+TestRunnable* ThreadTest::r__;
+zefiro_system::Thread* ThreadTest::thread__;
+int ThreadTest::count__ = 0;
+bool ThreadTest::active__ = false;
