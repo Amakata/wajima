a698048f0d4c3e8e6b172c692b8f232ab43c1f32
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-09-19 11:00:55 +0000

    ����������������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@213 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/include/lib/graphics/sys/D3DDevice.h
+++ wajima/include/lib/graphics/sys/D3DDevice.h
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/include/lib/graphics/sys/Attic/D3DDevice.h,v 1.3 2002/09/18 14:39:48 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/include/lib/graphics/sys/Attic/D3DDevice.h,v 1.4 2002/09/19 11:00:55 ama Exp $
  */
 #ifndef __D3DDEVICE8_H__
 #define __D3DDEVICE8_H__
@@ -9,6 +9,8 @@
 #include <string>
 
 #include "graphics/sys/D3DTexture.h"
+#include "graphics/sys/D3DFont.h"
+
 
 namespace zefiro_graphics {
 	class D3DDevice{
@@ -16,12 +18,14 @@ namespace zefiro_graphics {
 		D3DDevice( LPDIRECT3DDEVICE8 d3ddevice8 );
 		virtual ~D3DDevice();
 		D3DTexture *loadTexture( const std::string filename ) const;
+		D3DFont *createFont( const int width , const int height , std::string fontname );
 		void renderBegin();
 		void renderEnd();
-		void render( const D3DTexture *texture , float x ,float y , float z );
+		void render( const D3DTexture *texture , const float x , const float y , const float z );
 		void clear();
 	protected:
 		LPDIRECT3DDEVICE8	d3dDevice8_;
+		
 	};
 };
 
