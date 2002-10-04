89d429cf6b84556f36f5da7e61ed3144242c6429
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-10-04 13:58:23 +0000

    ������������������ ������������������������������������������������ ������������������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@230 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- zefirobenchmark/Device.h
+++ zefirobenchmark/Device.h
@@ -8,7 +8,7 @@
 #include <string>
 
 
-#define DXERROR_LOG( hr )	{	HRESULT hr2 = hr; if( hr2 != D3D_OK ){std::ofstream ofs;	ofs.open("error_log",std::ios_base::out | std::ios_base::app ); ofs <<__FILE__<<":"<<__LINE__<<":"<<hr2<< std::endl; ofs.close();} }
+#define DXERROR_LOG( hr )	{	HRESULT hr2 = hr; if( hr2 != D3D_OK ){std::ofstream ofs;	ofs.open("error_log.txt",std::ios_base::out | std::ios_base::app ); ofs <<__FILE__<<":"<<__LINE__<<":"<<hr2<< std::endl; ofs.close();} }
 
 class Device {
 public:
@@ -37,22 +37,258 @@ public:
 	void setRenderState( bool transparent ){
 		if( device_ != NULL ){
 			transparent_ = transparent;
+			DXERROR_LOG(device_->SetRenderState( D3DRS_SHADEMODE , D3DSHADE_FLAT ));
 			DXERROR_LOG(device_->SetRenderState( D3DRS_ZENABLE , D3DZB_TRUE   ));
 			DXERROR_LOG(device_->SetRenderState( D3DRS_LIGHTING , FALSE ));
 			DXERROR_LOG(device_->SetRenderState( D3DRS_ALPHAREF , 0 ));
 			DXERROR_LOG(device_->SetRenderState( D3DRS_ALPHAFUNC , D3DCMP_GREATER ));
 			DXERROR_LOG(device_->SetRenderState( D3DRS_ALPHATESTENABLE , TRUE ));
-			DXERROR_LOG(device_->SetRenderState( D3DRS_CULLMODE , TRUE ));
+			DXERROR_LOG(device_->SetRenderState( D3DRS_CULLMODE , D3DCULL_NONE ));
+			DXERROR_LOG(device_->SetRenderState( D3DRS_COLORVERTEX , FALSE ));
 			if( transparent ){
 				DXERROR_LOG(device_->SetRenderState( D3DRS_ZWRITEENABLE , FALSE ));
 				DXERROR_LOG(device_->SetRenderState( D3DRS_ALPHABLENDENABLE , TRUE ));
 				DXERROR_LOG(device_->SetRenderState( D3DRS_SRCBLEND , D3DBLEND_SRCALPHA ));
 				DXERROR_LOG(device_->SetRenderState( D3DRS_DESTBLEND , D3DBLEND_DESTALPHA ));
-				DXERROR_LOG(device_->SetRenderState( D3DRS_BLENDOP , D3DBLENDOP_ADD ));	
+				DXERROR_LOG(device_->SetRenderState( D3DRS_BLENDOP , D3DBLENDOP_ADD  ));	
 			}else{
 				DXERROR_LOG(device_->SetRenderState( D3DRS_ZWRITEENABLE , TRUE ));
 			}
 		}
+	}
+	void printRenderState(){
+		std::ofstream ofs;
+		ofs.open("state.txt", std::ios_base::out & std::ios_base::app );
+		if( device_ != NULL ){
+			DWORD v;
+			D3DLINEPATTERN pattern;
+			float f;
+			ofs << "D3DRS_ZENABLE = ";
+			device_->GetRenderState( D3DRS_ZENABLE , &v );
+			switch( v ){
+				case D3DZB_FALSE:
+					ofs << "D3DZB_FALSE";
+					break;
+				case D3DZB_TRUE:
+					ofs << "D3DZB_TRUE";
+					break;
+				case D3DZB_USEW:
+					ofs << "D3DZB_USEW";
+					break;
+			}
+			ofs << std::endl;
+			ofs << "D3DRS_FILLMODE = ";
+			device_->GetRenderState( D3DRS_FILLMODE , &v );
+			switch( v ){
+				case D3DFILL_POINT:
+					ofs << "D3DFILL_POINT";
+					break;
+				case D3DFILL_WIREFRAME:
+					ofs << "D3DFILL_WIREFRAME";
+					break;
+				case D3DFILL_SOLID:
+					ofs << " D3DFILL_SOLID";
+					break;
+			}
+			ofs << std::endl;
+			ofs << "D3DRS_SHADEMODE = ";
+			device_->GetRenderState( D3DRS_SHADEMODE, &v );
+			switch( v ){
+				case D3DSHADE_FLAT:
+					ofs << "D3DSHADE_FLAT";
+					break;
+				case D3DSHADE_GOURAUD:
+					ofs << "D3DSHADE_GOURAUD";
+					break;
+				case D3DSHADE_PHONG:
+					ofs << "D3DSHADE_PHONG";
+					break;
+			}
+			ofs << std::endl;
+			device_->GetRenderState( D3DRS_LINEPATTERN , (DWORD *)&pattern );
+			ofs << "wRepeatPattern = " << pattern.wRepeatFactor << " wLinePattern = " << pattern.wLinePattern << std::endl;
+			device_->GetRenderState( D3DRS_ZWRITEENABLE , &v );
+			ofs << "D3DRS_ZWRITEENABLE = " << getBool( v ) << std::endl;
+			device_->GetRenderState( D3DRS_ALPHATESTENABLE , &v );
+			ofs << "D3DRS_ALPHATESTENABLE = " << getBool( v ) << std::endl;
+			device_->GetRenderState( D3DRS_LASTPIXEL , &v );
+			ofs << "D3DRS_LASTPIXEL = " << getBool( v ) << std::endl;
+			ofs << "D3DRS_SRCBLEND = ";
+			device_->GetRenderState( D3DRS_SRCBLEND , &v );
+			ofs << getD3DBlend( v ) << std::endl;
+			ofs << "D3DRS_DESTBLEND = ";
+			device_->GetRenderState( D3DRS_DESTBLEND , &v );
+			ofs << getD3DBlend( v ) << std::endl;
+			ofs << "D3DRS_CULLMODE = ";
+			device_->GetRenderState( D3DRS_CULLMODE , &v );
+			switch( v ){
+				case D3DCULL_NONE:
+					ofs << "D3DCULL_NONE";
+					break;
+				case D3DCULL_CW:
+					ofs << "D3DCULL_CW";
+					break;
+				case D3DCULL_CCW:
+					ofs << "D3DCULL_CCW";
+					break;
+			}
+			ofs << std::endl;
+			ofs << "D3DRS_ZFUNC = ";
+			device_->GetRenderState( D3DRS_ZFUNC , &v );
+			ofs << getCmpFunc( v) << std::endl;
+			device_->GetRenderState( D3DRS_ALPHAREF , &v );
+			ofs << "D3DRS_ALPHAREF = " << v << std::endl;
+			device_->GetRenderState( D3DRS_DITHERENABLE , &v );
+			ofs << "D3DRS_DITHERENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_ALPHABLENDENABLE , &v );
+			ofs << "D3DRS_ALPHABLENDENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_FOGENABLE , &v );
+			ofs << "D3DRS_FOGENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_SPECULARENABLE , &v );
+			ofs << "D3DRS_SPECULARENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_FOGCOLOR , &v );
+			ofs << "D3DRS_FOGCOLOR = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_FOGTABLEMODE , &v );
+			ofs << "D3DRS_FOGTABLEMODE = " << getFogMode(v) << std::endl;
+			device_->GetRenderState( D3DRS_FOGSTART , (DWORD*)&f );
+			ofs <<  "D3DRS_FOGSTART = " << f << std::endl;
+			device_->GetRenderState( D3DRS_FOGEND , (DWORD*)&f );
+			ofs <<  "D3DRS_FOGEND = " << f << std::endl;
+			device_->GetRenderState( D3DRS_FOGDENSITY , (DWORD*)&f );
+			ofs <<  "D3DRS_FOGDENSITY = " << f << std::endl;
+			device_->GetRenderState( D3DRS_EDGEANTIALIAS , &v );
+			ofs << "D3DRS_EDGEANTIALIAS = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_ZBIAS , &v );
+			ofs << "D3DRS_ZBIAS = " << v << std::endl;
+			device_->GetRenderState( D3DRS_RANGEFOGENABLE , &v );
+			ofs << "D3DRS_RANGEFOGENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_STENCILENABLE  , &v );
+			ofs << "D3DRS_STENCILENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_STENCILFAIL , &v );
+			ofs << "D3DRS_STENCILFAIL = " << getStencilop(v) << std::endl;
+			device_->GetRenderState( D3DRS_STENCILZFAIL , &v );
+			ofs << "D3DRS_STENCILZFAIL = " << getStencilop(v) << std::endl;
+			device_->GetRenderState( D3DRS_STENCILPASS , &v );
+			ofs << "D3DRS_STENCILPASS = " << getStencilop(v) << std::endl;
+			device_->GetRenderState( D3DRS_STENCILFUNC , &v );
+			ofs << "D3DRS_STENCILFUNC = " << getCmpFunc(v) << std::endl;
+			device_->GetRenderState( D3DRS_STENCILREF , &v );
+			ofs << "D3DRS_STENCILREF = " << v << std::endl;
+			device_->GetRenderState( D3DRS_STENCILMASK , &v );
+			ofs << "D3DRS_STENCILMASK = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_STENCILWRITEMASK , &v );
+			ofs << "D3DRS_STENCILWRITEMASK = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_TEXTUREFACTOR , &v );
+			ofs << "D3DRS_TEXTUREFACTOR = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_WRAP0 , &v );
+			ofs << "D3DRS_WRAP0 = " << getWrap(v) << std::endl;
+			device_->GetRenderState( D3DRS_CLIPPING , &v );
+			ofs << "D3DRS_CLIPPING = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_LIGHTING , &v );
+			ofs << "D3DRS_LIGHTING = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_AMBIENT , &v );
+			ofs << "D3DRS_AMBIENT = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_FOGVERTEXMODE  , &v );
+			ofs << "D3DRS_FOGVERTEXMODE = " << getFogMode(v) << std::endl;
+			device_->GetRenderState( D3DRS_COLORVERTEX , &v );
+			ofs << "D3DRS_COLORVERTEX = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_LOCALVIEWER , &v );
+			ofs << "D3DRS_LOCLAVIEWR = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_NORMALIZENORMALS , &v );
+			ofs << "D3DRS_NORMALIZENORMALS = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_DIFFUSEMATERIALSOURCE , &v );
+			ofs << "D3DRS_DIFFUSEMATERIALSOURCE = " << getMaterialColorSource(v) << std::endl;
+			device_->GetRenderState( D3DRS_SPECULARMATERIALSOURCE , &v );
+			ofs << "D3DRS_SPECULARMATERIALSOURCE = " << getMaterialColorSource(v) << std::endl;
+			device_->GetRenderState( D3DRS_AMBIENTMATERIALSOURCE , &v );
+			ofs << "D3DRS_AMBIENTMATERIALSOURCE = " << getMaterialColorSource(v) << std::endl;
+			device_->GetRenderState( D3DRS_EMISSIVEMATERIALSOURCE , &v );
+			ofs << "D3DRS_EMISSIVEMATERIALSOURCE = " << getMaterialColorSource(v) << std::endl;
+			device_->GetRenderState(  D3DRS_VERTEXBLEND , &v );
+			ofs << "D3DRS_VERTEXBLEND = ";
+			switch( v ){
+				case D3DVBF_DISABLE:
+					ofs << "D3DVBF_DISABLE";
+					break;
+				case D3DVBF_1WEIGHTS:
+					ofs << "D3DVBF_1WEIGHTS";
+					break;
+				case D3DVBF_2WEIGHTS:
+					ofs << "D3DVBF_2WEIGHTS";
+					break;
+				case D3DVBF_3WEIGHTS:
+					ofs << "D3DVBF_3WEIGHTS";
+					break;
+				case D3DVBF_TWEENING:
+					ofs << "D3DVBF_TWEENING";
+					break;
+				case D3DVBF_0WEIGHTS:
+					ofs << "D3DVBF_0WEIGHTS";
+					break;
+			}
+			ofs << std::endl;
+			device_->GetRenderState( D3DRS_CLIPPLANEENABLE, &v );
+			ofs << "D3DRS_CLIPPLANEENABLE = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_SOFTWAREVERTEXPROCESSING , &v );
+			ofs << "D3DRS_SOFTWAREVERTEXPROCESSING = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_POINTSIZE , (DWORD*)&f );
+			ofs << "D3DRS_POINTSIZE = " << f << std::endl;
+			device_->GetRenderState( D3DRS_POINTSIZE_MIN , (DWORD*)&f );
+			ofs << "D3DRS_POINTSIZE_MIN = " << f << std::endl;
+			device_->GetRenderState( D3DRS_POINTSIZE_MAX , (DWORD*)&f );
+			ofs << "D3DRS_POINTSIZE_MAX = " << f << std::endl;
+			device_->GetRenderState( D3DRS_POINTSPRITEENABLE , &v );
+			ofs << "D3DRS_POINTSPRITEENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_POINTSCALEENABLE , &v );
+			ofs << "D3DRS_POINTSCALEENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_POINTSCALE_A , (DWORD*)&f );
+			ofs << "D3DRS_POINTSCALE_A = " << f << std::endl;
+			device_->GetRenderState( D3DRS_POINTSCALE_B , (DWORD*)&f );
+			ofs << "D3DRS_POINTSCALE_B = " << f << std::endl;
+			device_->GetRenderState( D3DRS_POINTSCALE_C , (DWORD*)&f );
+			ofs << "D3DRS_POINTSCALE_C = " << f << std::endl;
+			device_->GetRenderState( D3DRS_MULTISAMPLEANTIALIAS , &v );
+			ofs << "D3DRS_MULTISAMPLEANTIALIAS = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_MULTISAMPLEMASK, &v );
+			ofs << "D3DRS_MULTISAMPLEMASK = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_PATCHEDGESTYLE , &v );
+			ofs << "D3DRS_PATCHEDGESTYLE = ";
+			switch( v ){
+				case D3DPATCHEDGE_DISCRETE:
+					ofs << "D3DPATCHEDGE_DISCRETE";
+					break;
+				case D3DPATCHEDGE_CONTINUOUS:
+					ofs << "D3DPATCHEDGE_CONTINUOUS";
+					break;
+			}
+			ofs << std::endl;
+			device_->GetRenderState( D3DRS_PATCHSEGMENTS , (DWORD*)&f );
+			ofs << "D3DRS_PATCHSEGMENTS = " << f << std::endl;
+			device_->GetRenderState( D3DRS_DEBUGMONITORTOKEN , &v );
+			ofs << "D3DRS_DEBUGMONITORTOKEN = ";
+			switch( v ){
+				case D3DDMT_ENABLE:
+					ofs << "D3DDMT_ENABLE";
+					break;
+				case D3DDMT_DISABLE:
+					ofs << "D3DDMT_DISABLE";
+					break;
+				default:
+					ofs << "ERROR";
+					break;
+			}
+			ofs << std::endl;
+			device_->GetRenderState( D3DRS_INDEXEDVERTEXBLENDENABLE , &v );
+			ofs << "D3DRS_INDEXEDVERTEXBLENDENABLE = " << getBool(v) << std::endl;
+			device_->GetRenderState( D3DRS_COLORWRITEENABLE , &v );
+			ofs << "D3DRS_COLORWRITEENABLE = " << std::hex << v << std::dec << std::endl;
+			device_->GetRenderState( D3DRS_TWEENFACTOR , (DWORD*)&f );
+			ofs << "D3DRS_TWEENFACTOR = " << f << std::endl;
+			device_->GetRenderState( D3DRS_BLENDOP , &v );
+			ofs << "D3DRS_BLENDOP = " << getD3DBlend( v ) << std::endl;
+
+		}
+		ofs.close();
 	}
 	VertexBuffer *createVertexBuffer( int spriteSize ){
 		return new VertexBuffer( device_ , spriteSize );
@@ -113,10 +349,6 @@ protected:
 	void createDevice( HWND hWnd ,  LPDIRECT3D8 &d3d , bool windowed ){
 		D3DDISPLAYMODE mode;
 		DXERROR_LOG(d3d->GetAdapterDisplayMode( D3DADAPTER_DEFAULT , &mode ));
-		if( mode.Format != D3DFMT_X8R8G8B8 && windowed ){
-			MessageBox( hWnd , "ウィンドウモードで実行する場合は２４ビットモードで実行してください。","お願い",MB_OK);
-			return;
-		}
 		D3DPRESENT_PARAMETERS d3dpp;
 		ZeroMemory( &d3dpp, sizeof(d3dpp) );
 		d3dpp.Windowed   = windowed;
@@ -131,7 +363,42 @@ protected:
 		hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &device_ );
 		DXERROR_LOG( hr );
 		if( hr != D3D_OK ){
-			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		d3dpp.BackBufferFormat = D3DFMT_R8G8B8;
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		d3dpp.BackBufferFormat = D3DFMT_R5G6B5;
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_REF, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &device_ );
+			DXERROR_LOG( hr );
+		}
+		d3dpp.BackBufferFormat = D3DFMT_X1R5G5B5;
+		if( hr != D3D_OK ){
+			hr = d3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &d3dpp, &device_ );
 			DXERROR_LOG( hr );
 		}
 		if( hr != D3D_OK ){
@@ -155,6 +422,8 @@ protected:
 	}
 	void setTextureStageState(){
 		if( device_ != NULL ){
+			DXERROR_LOG(device_->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_SELECTARG1 ));
+			DXERROR_LOG(device_->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE));
 			DXERROR_LOG(device_->SetTextureStageState( 0 , D3DTSS_MIPFILTER , D3DX_FILTER_LINEAR ));
 			DXERROR_LOG(device_->SetTextureStageState( 0, D3DTSS_MAGFILTER, D3DX_FILTER_LINEAR ));
 			DXERROR_LOG(device_->SetTextureStageState( 0, D3DTSS_MINFILTER, D3DX_FILTER_LINEAR  ));
@@ -185,6 +454,153 @@ protected:
 	void releaseFont(){
 		if( font_ != NULL ){
 			font_->Release();
+		}
+	}
+	std::string getD3DBlend( DWORD v ){
+		std::ostringstream oss;
+		switch( v ){
+		case D3DBLEND_ZERO :
+			oss << "D3DBLEND_ZERO";
+			break;
+		case D3DBLEND_ONE:
+			oss << "D3DBLEND_ONE";
+			break;
+		case D3DBLEND_SRCCOLOR:
+			oss << "D3DBLEND_SRCCOLOR";
+			break;
+		case D3DBLEND_INVSRCCOLOR:
+			oss << "D3DBLEND_INVSRCCOLOR";
+			break;
+		case D3DBLEND_SRCALPHA:
+			oss << "D3DBLEND_SRCALPHA";
+			break;
+		case D3DBLEND_INVSRCALPHA:
+			oss << "D3DBLEND_INVSRCALPHA";
+			break;
+		case D3DBLEND_DESTALPHA:
+			oss << "D3DBLEND_DESTALPHA";
+			break;
+		case D3DBLEND_INVDESTALPHA:
+			oss << "D3DBLEND_INVDESTALPHA";
+			break;
+		case D3DBLEND_DESTCOLOR:
+			oss << "D3DBLEND_DESTCOLOR";
+			break;
+		case D3DBLEND_INVDESTCOLOR:
+			oss << "D3DBLEND_INVDESTCOLOR";
+			break;
+		case D3DBLEND_SRCALPHASAT :
+			oss << "D3DBLEND_SRCALPHASAT ";
+			break;
+		case D3DBLEND_BOTHSRCALPHA:
+			oss << "D3DBLEND_BOTHSRCALPHA";
+			break;
+		case D3DBLEND_BOTHINVSRCALPHA:
+			oss << "D3DBLEND_BOTHINVSRCALPHA";
+			break;
+		}
+		return oss.str();
+	}
+	std::string getCmpFunc( DWORD v ){
+		std::ostringstream oss;
+		switch( v ){
+			case D3DCMP_NEVER:
+				oss << "D3DCMP_NEVER";
+				break;
+			case D3DCMP_LESS:
+				oss << "D3DCMP_LESS";
+				break;
+			case D3DCMP_EQUAL:
+				oss << "D3DCMP_EQUAL";
+				break;
+			case D3DCMP_LESSEQUAL:
+				oss << "D3DCMP_LESSEQUAL";
+				break;
+			case D3DCMP_GREATER:
+				oss << "D3DCMP_GREATER";
+				break;
+			case D3DCMP_NOTEQUAL:
+				oss << "D3DCMP_NOTEQUAL";
+				break;
+			case D3DCMP_GREATEREQUAL:
+				oss << "D3DCMP_GREATEREQUAL";
+				break;
+			case D3DCMP_ALWAYS:
+				oss << "D3DCMP_ALWAYS";
+				break;
+		}
+		return oss.str();
+	}
+	std::string getBool( DWORD v ){
+		if( v == TRUE ){
+			return std::string("TRUE");
+		}else{
+			return std::string("FALSE");
+		}
+	}
+	std::string getStencilop( DWORD v ){
+		switch(v){
+			case D3DSTENCILOP_KEEP:
+				return std::string("D3DSTENCILOP_KEEP");
+			case D3DSTENCILOP_ZERO:
+				return std::string("D3DSTENCILOP_ZERO");
+			case D3DSTENCILOP_REPLACE:
+				return std::string("D3DSTENCILOP_REPLACE");
+			case D3DSTENCILOP_INCRSAT:
+				return std::string("D3DSTENCILOP_INCRSAT");
+			case D3DSTENCILOP_DECRSAT:
+				return std::string("D3DSTENCILOP_DECRSAT");
+			case D3DSTENCILOP_INVERT:
+				return std::string("D3DSTENCILOP_INVERT");
+			case D3DSTENCILOP_INCR:
+				return std::string("D3DSTENCILOP_INCR");
+			case D3DSTENCILOP_DECR:
+				return std::string("D3DSTENCILOP_DECR");
+			default:
+				return "";
+		}
+	}
+	std::string getWrap( DWORD v ){
+		std::ostringstream oss;
+		
+		if( v & D3DWRAPCOORD_0 ){
+			oss << "D3DWRAP_U ";
+		}
+		if( v & D3DWRAPCOORD_1 ){
+			oss << "D3DWRAP_V ";
+		}
+		if( v & D3DWRAPCOORD_2 ){
+			oss << "D3DWRAP_W ";
+		}
+		if( v & D3DWRAPCOORD_3 ){
+			oss << "D3DWRAP_X ";
+		}
+		return oss.str();
+	}
+	std::string getFogMode( DWORD v ){
+		switch( v ){
+			case D3DFOG_NONE:
+				return "D3DFOG_NONE";
+			case D3DFOG_EXP:
+				return "D3DFOG_EXP";
+			case D3DFOG_EXP2:
+				return  "D3DFOG_EXP2";
+			case D3DFOG_LINEAR:
+				return "D3DFOG_LINEAR";
+			default:
+				return "";
+		}
+	}
+	std::string getMaterialColorSource( DWORD v ){
+		switch( v ){
+			case D3DMCS_MATERIAL:
+				return "D3DMCS_MATERIAL";
+			case D3DMCS_COLOR1:
+				return "D3DMCS_COLOR1";
+			case D3DMCS_COLOR2:
+				return "D3DMCS_COLOR2";
+			default:
+				return "";
 		}
 	}
 	LPDIRECT3DDEVICE8 device_;
