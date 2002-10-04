ea591a42e7097546ebb77723c58bc8662cb6353f
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-10-04 13:45:47 +0000

    D3D������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@222 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- zefirobenchmark/D3D.h
+++ zefirobenchmark/D3D.h
@@ -8,6 +8,13 @@ class D3D {
 public:
 	D3D():d3d_(NULL){
 		d3d_ = Direct3DCreate8(D3D_SDK_VERSION);
+		if( d3d_ == NULL ){
+			std::ofstream ofs;
+			ofs.open("error_log",std::ios_base::out | std::ios_base::app );
+			ofs <<__FILE__<<":"<<__LINE__<< std::endl;
+			ofs.close();		
+
+		}
 	}
 	Device *createDevice( HWND hWnd , bool windowed ){
 		return new Device( hWnd , d3d_ , windowed );
