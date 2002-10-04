6b6a54d354f7450d2fdbacd2ca9a14083a9f1141
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-10-04 13:51:19 +0000

    �������������������������� ����������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@227 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- zefirobenchmark/VertexBuffer.h
+++ zefirobenchmark/VertexBuffer.h
@@ -3,19 +3,28 @@
 #include "vertex.h"
 #include "texture.h"
 
+#define DXERROR_LOG( hr )	{	HRESULT hr2 = hr; if( hr2 != D3D_OK ){std::ofstream ofs;	ofs.open("error_log",std::ios_base::out | std::ios_base::app ); ofs <<__FILE__<<":"<<__LINE__<<":"<<hr2<< std::endl; ofs.close();}  }
+
+
 class VertexBuffer {
 public:
 	VertexBuffer( LPDIRECT3DDEVICE8 device , int spriteSize ):vb_(NULL),cv_(NULL),spriteSize_(spriteSize){
-		device->CreateVertexBuffer( sizeof(CUSTOMVERTEX) * spriteSize * 6 , 0 , D3DFVF_CUSTOMVERTEX , D3DPOOL_MANAGED , &vb_ );
+		DXERROR_LOG(device->CreateVertexBuffer( sizeof(CUSTOMVERTEX) * spriteSize * 6 , 0 , D3DFVF_CUSTOMVERTEX , D3DPOOL_MANAGED , &vb_ ));
 	}
 	void lock(){
 		if( vb_ != NULL && cv_ == NULL ){
-			vb_->Lock( 0 , 0 , (BYTE**)&cv_ , D3DLOCK_DISCARD );
+			HRESULT hr;
+			hr = vb_->Lock( 0 , 0 , (BYTE**)&cv_ , D3DLOCK_DISCARD );
+			DXERROR_LOG(hr);
+			if( D3D_OK != hr ){
+				hr = vb_->Lock( 0 , 0 , (BYTE**)&cv_ , 0 );
+				DXERROR_LOG(hr);
+			}
 		}
 	}
 	void unlock(){
 		if( vb_ != NULL && cv_ != NULL ){
-			vb_->Unlock();
+			DXERROR_LOG((vb_->Unlock()));
 			cv_ = NULL;
 		}
 	}
