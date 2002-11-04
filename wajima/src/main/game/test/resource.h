3628770b6046ce91692bf14bafc4f027f79700f4
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-11-04 15:49:49 +0000

    test������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@245 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/main/game/test/resource.h
+++
@@ -1,7 +0,0 @@
-/**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/main/game/test/Attic/resource.h,v 1.1 2002/11/03 09:07:20 ama Exp $
- */
-
-#define IDM_EXIT	100
-#define IDM_TEST	200
-#define IDM_GAME_START	300
