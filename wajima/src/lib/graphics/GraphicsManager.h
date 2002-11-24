cf0e3dc59b9604b88093b84e74b12d235115e713
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-11-24 14:51:06 +0000

    ����������������������������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@281 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/GraphicsManager.h
+++ wajima/src/lib/graphics/GraphicsManager.h
@@ -1,20 +1,37 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsManager.h,v 1.1 2002/11/13 18:28:08 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsManager.h,v 1.2 2002/11/24 14:51:06 ama Exp $
  */
 #ifndef __GRAPHICSMANAGER_H__
 #define __GRAPHICSMANAGER_H__
 
 #include <windows.h>
 #include <d3d8.h>
+
+#include <vector>
+#include <functional>
+
 #include "SmartPtr.h"
-#include "graphics/GraphicsDevice.h"
+#include "GraphicsDevice.h"
+#include "GraphicsDeviceMode.h"
+#include "ColorFormat.h"
 
 namespace zefiro_graphics {
+	class GraphicsDeviceModeFilter : public std::unary_function<std::vector<GraphicsDeviceMode>,std::vector<GraphicsDeviceMode> > {
+	public:
+		virtual result_type operator()(argument_type gdms){
+			return (result_type)gdms;
+		}
+	};
+
 	class GraphicsManager {
 	public:
-		static Loki::SmartPtr<GraphicsManager> createGM( HWND );
+		typedef Loki::SmartPtr<GraphicsManager> GM;
+		typedef std::vector<GraphicsDeviceMode> GDM_CONTAINERd;
+		static GM createGM( HWND hwnd );
 		virtual ~GraphicsManager();
-		Loki::SmartPtr<GraphicsDevice> createGD( int , int , int , bool );
+		GraphicsDevice::GD createGD( int width , int height , ColorFormat , int refreshRate , bool windowed );
+		GraphicsDevice::GD createGD( GraphicsDeviceMode gdm );
+		std::vector<GraphicsDeviceMode> queryGD( std::unary_function<std::vector<GraphicsDeviceMode>,std::vector<GraphicsDeviceMode> > &queryFunc = AllGraphicsDevice() );
 	protected:
 		GraphicsManager( HWND );
 		LPDIRECT3D8	d3d_;
