ec463bfdfe6babf33e8cb73a1b34f6c7f5194cf4
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-11-24 14:54:21 +0000

    ColorFormat������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@285 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/GraphicsDevice.h
+++ wajima/src/lib/graphics/GraphicsDevice.h
@@ -1,14 +1,18 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsDevice.h,v 1.2 2002/11/14 16:51:06 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsDevice.h,v 1.3 2002/11/24 14:54:21 ama Exp $
  */
 
 #ifndef __GRAPHICSDEVICE_H__
 #define __GRAPHICSDEVICE_H__
 
+#include "SmartPtr.h"
+#include "ColorFormat.h"
+
 namespace zefiro_graphics {
 	class GraphicsDevice {
 	public:
-		GraphicsDevice( LPDIRECT3D8 d3d8 , int , int , int , bool );
+		typedef Loki::SmartPtr<GraphicsDevice> GD;
+		GraphicsDevice( int width , int height , ColorFormat colorFormat , int refreshRate , bool windowed );
 	};
 };
 #endif //__GRAPHICSDEVICE_H__
