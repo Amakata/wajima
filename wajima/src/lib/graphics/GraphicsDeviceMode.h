64fb9b073e55bc39380d78bfabca2640dbfa7948
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-11-24 17:42:20 +0000

    adapterNumber������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@290 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/GraphicsDeviceMode.h
+++ wajima/src/lib/graphics/GraphicsDeviceMode.h
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsDeviceMode.h,v 1.1 2002/11/24 14:53:59 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsDeviceMode.h,v 1.2 2002/11/24 17:42:20 ama Exp $
  */
 #ifndef __GRAPHICSDEVICEMODE_H__
 #define __GRAPHICSDEVICEMODE_H__
@@ -9,8 +9,8 @@
 namespace zefiro_graphics {
 	class GraphicsDeviceMode {
 	public:
-		GraphicsDeviceMode( int width , int height , ColorFormat colorFormat , int refreshRate , bool windowed )
-			: width_(width),height_(height),colorFormat_(colorFormat),refreshRate_(refreshRate),windowed_(windowed){
+		GraphicsDeviceMode( int width , int height , ColorFormat colorFormat , int refreshRate , bool windowed , int adapterNumber )
+			: width_(width),height_(height),colorFormat_(colorFormat),refreshRate_(refreshRate),windowed_(windowed),adapterNumber_(adapterNumber){
 		}
 		int getWidth() const {
 			return width_;
@@ -27,12 +27,16 @@ namespace zefiro_graphics {
 		bool getWindowed() const{
 			return windowed_;
 		}
+		int getAdapterNumber() const{
+			return adapterNumber_;
+		}
 	private:
 		int width_;
 		int height_;
 		ColorFormat colorFormat_;
 		int refreshRate_;
 		bool windowed_;
+		int adapterNumber_;
 	};
 };
 
