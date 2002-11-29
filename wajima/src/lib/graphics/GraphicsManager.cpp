b48324a74ecb553035df47e28595926c2a0149d4
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-11-29 18:21:51 +0000

    queryGD createGD������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@299 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/GraphicsManager.cpp
+++ wajima/src/lib/graphics/GraphicsManager.cpp
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsManager.cpp,v 1.5 2002/11/26 11:26:24 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsManager.cpp,v 1.6 2002/11/29 18:21:51 ama Exp $
  */
 
 #include "GraphicsManager.h"
@@ -24,67 +24,5 @@ namespace zefiro_graphics {
 		if( d3d_ != NULL ){
 			d3d_->Release();
 		}
-	}
-	GraphicsDevice::GD GraphicsManager::createGD( int width , int height , ColorFormat colorFormat , int refreshRate , bool windowed , int adapterNumber ){
-		// TODO: GDの作成を正しくやるためのルーチンをかかねば・・・。しくしく。
-		D3DPRESENT_PARAMETERS d3dParams = {0};
-		d3dParams.Windowed = windowed;
-		d3dParams.SwapEffect = D3DSWAPEFFECT_DISCARD;
-		d3dParams.BackBufferHeight = height;
-		d3dParams.BackBufferWidth = width;
-		d3dParams.BackBufferCount = 1;
-		d3dParams.EnableAutoDepthStencil = TRUE;
-		d3dParams.FullScreen_RefreshRateInHz = refreshRate;
-
-		return GraphicsDevice::GD( NULL );
-	}
-	GraphicsDevice::GD GraphicsManager::createGD( GraphicsDeviceMode gdm ){
-		return createGD( gdm.getWidth() , gdm.getHeight() , gdm.getColorFormat() , gdm.getRefreshRate() , gdm.isWindowed() , gdm.getAdapterNumber() );
-	}
-	GraphicsDevice::GD GraphicsManager::createGD( GraphicsDeviceModeSelector &selector ){
-		return createGD( selector( queryGD() ) );
-	}
-	std::vector<GraphicsDeviceMode> GraphicsManager::queryGD( GraphicsDeviceModeFilter &filter ){
-		std::vector<GraphicsDeviceMode> result;
-		int adapterCountSize = d3d_->GetAdapterCount();
-		for( int adapterCount = 0 ; adapterCount < adapterCountSize ; ++adapterCount ){
-			int deviceCountSize = d3d_->GetAdapterModeCount( adapterCountSize );
-			for( int deviceCount = 0 ; deviceCount < deviceCountSize ; ++deviceCount ){
-				D3DDISPLAYMODE mode;
-				ColorFormat::FORMAT format;
-				DXASSERT( d3d_->EnumAdapterModes( adapterCount , deviceCount , &mode ));
-				switch( mode.Format ){
-					case D3DFMT_R8G8B8:
-						format = ColorFormat::R8G8B8;
-						break;
-					case D3DFMT_A8R8G8B8:
-						format = ColorFormat::A8R8G8B8;
-						break;
-					case D3DFMT_X8R8G8B8:
-						format = ColorFormat::X8R8G8B8;
-						break;
-					case D3DFMT_R5G6B5:
-						format = ColorFormat::R5G6B5;
-						break;
-					case D3DFMT_X1R5G5B5:
-						format = ColorFormat::X1R5G5B5;
-						break;
-					case D3DFMT_A1R5G5B5:
-						format = ColorFormat::A1R5G5B5;
-						break;
-					default:
-						continue;
-				}
-				if( D3D_OK == d3d_->CheckDeviceType( adapterCount , D3DDEVTYPE_HAL , mode.Format , mode.Format , false ) || 
-					D3D_OK == d3d_->CheckDeviceType( adapterCount , D3DDEVTYPE_REF , mode.Format , mode.Format , false ) ){
-					result.push_back( GraphicsDeviceMode( mode.Width , mode.Height , ColorFormat( format ) , mode.RefreshRate , false , adapterCount ) );
-				}
-				if( D3D_OK == d3d_->CheckDeviceType( adapterCount , D3DDEVTYPE_HAL , mode.Format , mode.Format , true ) || 
-					D3D_OK == d3d_->CheckDeviceType( adapterCount , D3DDEVTYPE_REF , mode.Format , mode.Format , true ) ){
-					result.push_back( GraphicsDeviceMode( mode.Width , mode.Height , ColorFormat( format ) , mode.RefreshRate , false , adapterCount ) );
-				}
-			}
-		}
-		return filter( result );
 	}
 };
--- wajima/src/lib/graphics/GraphicsManager.h
+++ wajima/src/lib/graphics/GraphicsManager.h
@@ -1,5 +1,5 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsManager.h,v 1.4 2002/11/25 13:06:56 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/GraphicsManager.h,v 1.5 2002/11/29 18:21:13 ama Exp $
  */
 #ifndef __GRAPHICSMANAGER_H__
 #define __GRAPHICSMANAGER_H__
@@ -7,37 +7,16 @@
 #include <windows.h>
 #include <d3d8.h>
 
-#include <vector>
-#include <functional>
-
 #include "SmartPtr.h"
 #include "GraphicsDevice.h"
-#include "GraphicsDeviceMode.h"
 #include "ColorFormat.h"
 
 namespace zefiro_graphics {
-	// queryGD用ファンクタ
-	class GraphicsDeviceModeFilter {
-	public:
-		virtual std::vector<GraphicsDeviceMode> operator()(std::vector<GraphicsDeviceMode> gdms){
-			return gdms;
-		}
-	};
-	// createGD用ファンクタ
-	class GraphicsDeviceModeSelector {
-	public:
-		virtual GraphicsDeviceMode operator()( std::vector<GraphicsDeviceMode> ) = 0;		
-	};
-
 	class GraphicsManager {
 	public:
 		typedef Loki::SmartPtr<GraphicsManager> GM;
 		static GM createGM( HWND hwnd );
 		virtual ~GraphicsManager();
-		GraphicsDevice::GD createGD( int width , int height , ColorFormat colorFormat , int refreshRate , bool windowed , int adapterNumber );
-		GraphicsDevice::GD createGD( GraphicsDeviceMode gdm );
-		GraphicsDevice::GD createGD( GraphicsDeviceModeSelector &selector );
-		std::vector<GraphicsDeviceMode> queryGD( GraphicsDeviceModeFilter &filter = GraphicsDeviceModeFilter() );
 	protected:
 		GraphicsManager( HWND );
 		LPDIRECT3D8	d3d_;
