be32e0543000ff8d372e16fdcae5e072e636e93e
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-12-13 18:25:13 +0000

    DepthFormat��������������������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@302 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/src/lib/graphics/DepthFormat.h
+++ wajima/src/lib/graphics/DepthFormat.h
@@ -1,38 +1,66 @@
 /**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/DepthFormat.h,v 1.1 2002/11/26 11:31:35 ama Exp $
+ * $Header: /home/zefiro/cvsrep/cpp/wajima/src/lib/graphics/DepthFormat.h,v 1.2 2002/12/13 18:25:13 ama Exp $
  */
-	#ifndef __DEPTHFORMAT_H__
+#ifndef __DEPTHFORMAT_H__
 #define __DEPTHFORMAT_H__
 
 namespace zefiro_graphics {
 	class DepthFormat {
 	public:
-		enum FORMAT { D16LOCK , D32 , D15S1 , D24S8 , D16 , D24X8 , D24X4S4 };
-		DepthFormat( FORMAT f ):format_(f){
+		enum FORMAT { D16_LOCKABLE , D32 , D15S1 , D24S8 , D16 , D24X8 , D24X4S4 };
+		DepthFormat( FORMAT format ):format_(format){
 		}
-		int getBitLength() const {
+		FORMAT getFormat() const{
+			return format_;
+		}
+		int getDepthBitLength() const{
 			switch( format_ ){
-				case D32:
-					return 32;
-				case D24S8:
-				case D24X8:
-				case D24X4S4:
-					return 24;
-				case D16LOCK:
-				case D16:
-					return 16;
-				case D15S1:
-					return 15;
-				default:
-					return 0;
+			case D32:
+				return 32;
+			case D24S8:
+			case D24X8:
+			case D24X4S4:
+				return 24;
+			case D16_LOCKABLE:
+			case D16:
+				return 16;
+			case D15S1:
+				return 15;
+			default:
+				return 0;
+			};
+		}
+		int getStencilBitLength() const{
+			switch( format_ ){
+			case D24S8:
+				return 8;
+			case D24X4S4:
+				return 4;
+			case D15S1:
+				return 1;
+			default:
+				return 0;
 			}
 		}
-		FORMAT getFormat() const {
-			return format_;
+		int getBitLength() const{
+			switch( format_ ){
+			case D32:
+			case D24S8:
+			case D24X8:
+			case D24X4S4:
+				return 32;
+			case D16_LOCKABLE:
+			case D16:
+			case D16S1:
+				return 16;
+			default:
+				return 0;
+			}
 		}
 	private:
 		FORMAT format_;
 	};
 };
+
 
 #endif //__DEPTHFORMAT_H__
