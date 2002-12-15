31e76a76e92ee264ae99449157cf307939729876
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2002-12-15 10:52:54 +0000

    present����������������������
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@315 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/test/lib/graphics/GraphicsManagerTest.cpp
+++ wajima/test/lib/graphics/GraphicsManagerTest.cpp
@@ -14,8 +14,6 @@ class GraphicsManagerTest : public CppUnit::TestFixture {
 
 private:
 	// your staff
-	typedef Loki::SmartPtr<zefiro_graphics::GraphicsManager> GM;
-	typedef Loki::SmartPtr<zefiro_graphics::GraphicsDevice> GD;
 public:
 	virtual void setUp() {
 		// initialize
@@ -28,24 +26,26 @@ public:
 //CUPPA:decl=+
     void test_createGM() {
 		try{
-			GM gm = zefiro_graphics::GraphicsManager::createGM( 0 );
+			zefiro_graphics::GraphicsManager::GM gm = zefiro_graphics::GraphicsManager::createGM( 0 );
 			CPPUNIT_FAIL("");
 		}catch( zefiro_std::InvalidArgument &e ){
 		}
 		try{
-			GM gm = zefiro_graphics::GraphicsManager::createGM( Config::getHWND() );
+			zefiro_graphics::GraphicsManager::GM gm = zefiro_graphics::GraphicsManager::createGM( Config::getHWND() );
 		}catch( zefiro_std::Exception &e ){
 			CPPUNIT_FAIL("");
 		}
 	}
     void test_createGD() {
-		GM gm;
+		zefiro_graphics::GraphicsManager::GM gm;
+		zefiro_graphics::GraphicsDevice::GD gd;
 		try{
 			gm = zefiro_graphics::GraphicsManager::createGM( Config::getHWND() );
 		}catch( zefiro_std::Exception &e ){
 			CPPUNIT_FAIL("");
 		}
 		try{
+			gd = gm->createGD( 800 , 600 , zefiro_graphics::ColorFormat::A8R8G8B8 , zefiro_graphics::DepthFormat::D16 , false  );
 		}catch( zefiro_std::InvalidArgument &e ){
 			CPPUNIT_FAIL("");
 		}
