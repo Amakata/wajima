6551df2250185778e87d3ab5908f8ebfeff0d816
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2004-10-23 02:06:04 +0000

    ファイル／フォルダを削除
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@331 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/test/main/game/resource.h
+++
@@ -1,7 +0,0 @@
-/**
- * $Header: /home/zefiro/cvsrep/cpp/wajima/test/main/game/resource.h,v 1.1 2002/11/04 16:28:14 ama Exp $
- */
-
-#define IDM_EXIT	100
-#define IDM_TEST	200
-#define IDM_GAME_START	300
