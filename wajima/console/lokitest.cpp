f368e2ffa23e4d2cf7e4792c3817165bc62f4e8e
ama <ama@0d65b87a-bfcd-0310-b916-82c895c6ade2>
2005-01-08 11:59:13 +0000

    WAFファイル生成ツールを作成した WAFStreamのテストコードを実行中
    git-svn-id: file:///Users/amakata/var/lib/svn/wajima/trunk@360 0d65b87a-bfcd-0310-b916-82c895c6ade2

--- wajima/console/lokitest.cpp
+++ wajima/console/lokitest.cpp
@@ -8,58 +8,87 @@
 #include <pss/std/Logger.hpp>
 #include <boost/lexical_cast.hpp>
 
-int main(int argc, char *argv[]) {
+/**
+ * wafbuf、wafstraem、ifstreamの動作チェック
+ */
+int test(int argc, char *argv[]) {
 	LOG4CXX_CONFIGUER("console.properties");
 	LOG4CXX_LOGGER_PTR log = LOG4CXX_LOGGER("main");
 	LOG4CXX_DEBUG(log, "begin.");
 	// ウィンドウズネームのみ許可
 	::boost::filesystem::path test("test", ::boost::filesystem::windows_name);
 	::boost::filesystem::path test2 = ::boost::filesystem::complete(test);
+	// pathの各種メソッドテスト
 	::std::cout << test2.root_path().string() << ::std::endl;
 	::std::cout << test2.root_name() << ::std::endl;
 	::std::cout << test2.root_directory() << ::std::endl;
 	::std::cout << test2.leaf() << ::std::endl;
 	::std::cout << test2.string() << ::std::endl;
-
+	
+	// waf用読み取りファイルバッファ
 	::pss::std::basic_waf_filebuf<char> wafbuf;
+	// waf入力ストリーム
 	::pss::std::basic_waf_ifstream<char> wafstream;
 	try {
+		//
+		// wafbufの動作を試す
+		//
+		// data.wafのtest2.txtをオープン
 		wafbuf.open("data/test2.txt");
+		// wafバッファを作成
 		::std::basic_istream<char> is(&wafbuf);
+
+		// 一行ずつ読み取り
 		::std::string str = "";
 		while (!is.eof()) {
 			::std::getline(is, str);
 			LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
 		}
+		// クリア
 		is.clear();
+		// 先頭へシーク
 		is.seekg(0, ::std::ios_base::beg);
 		::std::getline(is, str);
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
+		// 先頭から１バイトシーク
 		is.seekg(1, ::std::ios_base::beg);
 		::std::getline(is, str);
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
+		// 先頭から８バイトシーク
 		is.seekg(8, ::std::ios_base::beg);
 		::std::getline(is, str);
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
+		// 先頭から９バイトシーク
 		is.seekg(9, ::std::ios_base::beg);
 		::std::getline(is, str);
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
+		// 先頭から１０バイトシーク
 		is.seekg(10, ::std::ios_base::beg);
 		::std::getline(is, str);
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
 		is.clear();
+		// 現在位置から-11バイトシーク
 		is.seekg(-11, ::std::ios_base::cur);
 		::std::getline(is, str);
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
 		
 		wafbuf.close();
+
+		//
+		// wafstreamの動作を試す
+		//
+		// data.wafのtest2.txtをオープン
 		wafstream.open(::boost::filesystem::path("data/test2.txt"));
 		wafstream >>  str;
 		LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));		
 		wafstream.close();
 
-
+		//
+		//　ifstreamでwafbufのストリームと同じ動作をするか検証する
+		//
+		// test.txtをオープン
 		::std::basic_ifstream<char> ifs("test.txt");
+		// 一行ずつ読み込み
 		while (!ifs.eof()) {
 			::std::getline(ifs, str);
 			LOG4CXX_DEBUG(log, "str = " + str + " , length = " + ::boost::lexical_cast<::std::string>(str.length()));
@@ -82,4 +111,25 @@ int main(int argc, char *argv[]) {
 	}
 	LOG4CXX_DEBUG(log, "end.");
 	return 0;
+}
+
+int main(int argc, char* argv[]) {
+	LOG4CXX_CONFIGUER("console.properties");
+	LOG4CXX_LOGGER_PTR log = LOG4CXX_LOGGER("main");
+	LOG4CXX_DEBUG(log, "begin.");
+	::pss::std::basic_waf_ifstream<char> ws1,ws2;
+	ws1.open("data/hogehoge.txt");
+	ws1.seekg(0, ::std::io_base::end);
+	int size1 = ws1.tellg();
+	LOG4CXX_DEBUG(log, "size1 = " + ::boost::lexical_cast<::std::string>(size1));
+	::std::ofstream ofs("hogehoge.txt", ::std::ios_base::out | ::std::ios_base::binary);
+	for (int i = 0; i < size1 ; ++i) {
+		char buf;
+		ws1.read(&buf, 1);
+		ofs.write(&buf, 1);
+	}
+	ws1.close();
+
+
+	LOG4CXX_DEBUG(log, "end.");
 }
--- wajima/waftool/src/waftool.cpp
+++ wajima/waftool/src/waftool.cpp
@@ -8,45 +8,37 @@
 #include <pss/std/Logger.hpp>
 #include <boost/lexical_cast.hpp>
 
+
 /**
- * data/test/
- * 
+ * @param propertyFilePathStr WAFアーカイブファイルを作成するプロパティファイル
+ * @return 0 正常
+ *         -1 失敗
  */
-int main(int argc, char* argv[]) try {
-	LOG4CXX_CONFIGUER("console.properties");
-	LOG4CXX_LOGGER_PTR log = LOG4CXX_LOGGER("main");
+int doArchive(const ::std::string &propertyFilePathStr) {
+	LOG4CXX_LOGGER_PTR log = LOG4CXX_LOGGER("doArchive");
 	LOG4CXX_DEBUG(log, "begin.");
-
-	if (argc == 1) {
-		::std::cout << "this program is Wajima Archive File Tool." << ::std::endl;
-		::std::cout << "> waftool.exe [waf property file]" << ::std::endl;
-		return 0;
-	}
-	// 設定ファイルパス
-	::std::string propertyFilePath(argv[1]);
-
-	::std::ifstream ifs(propertyFilePath.c_str());
+	::std::ifstream ifs(propertyFilePathStr.c_str());
 
 	if (ifs.eof()) {
 		::std::cout << "waf property file is syntax error." << ::std::endl;
 		return -1;
 	}
 
-	::std::string wafFilePath = "";
+	::std::string wafFilePathStr = "";
 	// 一行目から、WAFファイルの名前を取得
-	::std::getline(ifs, wafFilePath);
-	::std::ofstream ofs(wafFilePath.c_str() , ::std::ios_base::binary | ::std::ios_base::out);
+	::std::getline(ifs, wafFilePathStr);
+	::std::ofstream ofs(wafFilePathStr.c_str() , ::std::ios_base::binary | ::std::ios_base::out);
 
-	::std::cout << "load <" + propertyFilePath + ">" << ::std::endl;
-	::std::cout << "waf file <" + wafFilePath + ">" << ::std::endl;
-
+	::std::cout << "load <" + propertyFilePathStr + ">" << ::std::endl;
+	::std::cout << "waf file <" + wafFilePathStr + ">" << ::std::endl;
+	int wafFileSize = 0;
 	while (!ifs.eof()) {
 		// アーカイブに登録するファイルをオープン
-		::std::string sourceFilePath;
-		::std::getline(ifs, sourceFilePath);
-		::std::cout << "source file <" + sourceFilePath + ">" << ::std::endl;
+		::std::string sourceFilePathStr;
+		::std::getline(ifs, sourceFilePathStr);
+		::std::cout << "source file <" + sourceFilePathStr + ">" << ::std::endl;
 
-		::std::ifstream sourcefs(sourceFilePath.c_str(), ::std::ios_base::binary | ::std::ios_base::in);
+		::std::ifstream sourcefs(sourceFilePathStr.c_str(), ::std::ios_base::binary | ::std::ios_base::in);
 
 		::pss::std::basic_waf_filebuf<char>::Header header = {0};
 		// ファイルサイズ
@@ -58,17 +50,103 @@ int main(int argc, char* argv[]) try {
 		header.mode_ = ::pss::std::basic_waf_filebuf<char>::WAF_NORMAL;
 		LOG4CXX_DEBUG(log, "mode = " + ::boost::lexical_cast<::std::string>(header.mode_));
 		// ファイル名(ディレクトリを除去、最後尾のファイル名のみ)
-		::std::string sourceFileName = ::boost::filesystem::basename(::boost::filesystem::path(sourceFilePath, ::boost::filesystem::no_check));
-		LOG4CXX_DEBUG(log, "leaf ok");
-		sourceFileName.copy(header.filename_, sourceFileName.length(), 0);
+		::boost::filesystem::path sourceFilePath(sourceFilePathStr, ::boost::filesystem::no_check);
+		::std::string sourceFilenameStr = *(--sourceFilePath.end()); 
+		sourceFilenameStr.copy(header.filename_, sourceFilenameStr.length(), 0);
 		LOG4CXX_DEBUG(log, "file = " + ::boost::lexical_cast<::std::string>(header.filename_));
 		ofs.write((char*) &header, sizeof(header));
-		::std::copy(::std::istream_iterator<char>(sourcefs), ::std::istream_iterator<char>(), ::std::ostream_iterator<char>(ofs));
+		LOG4CXX_DEBUG(log, "header size = " + ::boost::lexical_cast<::std::string>(sizeof(header)));
+		wafFileSize += sizeof(header);
+		sourcefs.seekg(0, ::std::ios_base::beg);
+		int count = 0;
+		for( int i = 0; i < header.size_ ; ++i, ++count ) {
+			char buf = 0;
+			sourcefs.read(&buf, 1);
+			ofs.write(&buf, 1);
+		}
+		wafFileSize += header.size_;
+		LOG4CXX_DEBUG(log, "write byte = " + ::boost::lexical_cast<::std::string>(count));		
 	}
+	LOG4CXX_DEBUG(log, "waf file size = " + ::boost::lexical_cast<::std::string>(wafFileSize));		
+	ifs.close();
+	ofs.flush();
+	ofs.close();
+	LOG4CXX_DEBUG(log,"end");
+	return 0;
+}
 
+/**
+ * @param wafFilePathStr
+ * @return -1 失敗
+ *         0 成功
+ */
+int doExpand(const ::std::string &wafFilePathStr) {
+	LOG4CXX_LOGGER_PTR log = LOG4CXX_LOGGER("doExpand");
+	LOG4CXX_DEBUG(log, "begin.");
+	::std::ifstream ifs(wafFilePathStr.c_str(), ::std::ios_base::binary | ::std::ios_base::in);
+	
+	// WAFファイル名に対応したディレクトリ作成
+	::boost::filesystem::path wafFilePath(wafFilePathStr, ::boost::filesystem::no_check);
+	::std::string wafFilenameStr = *(--wafFilePath.end());
+	LOG4CXX_DEBUG(log, wafFilenameStr);
+	::std::string directoryNameStr = ::boost::filesystem::basename(wafFilenameStr);
+	LOG4CXX_DEBUG(log, directoryNameStr);
+	if (!::boost::filesystem::exists(directoryNameStr)) {
+		::boost::filesystem::create_directory(directoryNameStr);
+	}
+	while (!ifs.eof()) {
+		// ヘッダ読み込み
+		::pss::std::basic_waf_filebuf<char>::Header header = {0};
+		ifs.read((char*) &header, sizeof(header));
+		LOG4CXX_DEBUG(log, "header.size = " + ::boost::lexical_cast<::std::string>(header.size_));		
+		LOG4CXX_DEBUG(log, "header.mode = " + ::boost::lexical_cast<::std::string>(header.mode_));		
+		LOG4CXX_DEBUG(log, "header.mode = " + ::boost::lexical_cast<::std::string>(header.filename_));		
+		::std::ofstream ofs((directoryNameStr + "/" + header.filename_).c_str(), ::std::ios_base::out | ::std::ios_base::binary);
+		for (int i = 0; i < header.size_; ++i) {
+			char buf;
+			ifs.read(&buf, 1);
+			ofs.write(&buf, 1);
+		}
+		ofs.close();
+	}
 	ifs.close();
 	LOG4CXX_DEBUG(log, "end.");
 	return 0;
+}
+
+/**
+ * @param argc
+ * @param argv
+ * @return 
+ */
+int main(int argc, char* argv[]) try {
+	LOG4CXX_CONFIGUER("console.properties");
+	LOG4CXX_LOGGER_PTR log = LOG4CXX_LOGGER("main");
+	LOG4CXX_DEBUG(log, "begin.");
+
+	if (argc != 3) {
+		::std::cout << "this program is Wajima Archive File Tool." << ::std::endl;
+		::std::cout << "> waftool.exe create [waf property file]" << ::std::endl;
+		::std::cout << "> waftool.exe expand [waf file]" << ::std::endl;
+		return 0;
+	}
+	::std::string command(argv[1]);
+	int result = 0;
+	if (command.compare("create") == 0) {
+		// WAFファイルぼ生成
+		// 設定ファイルパス
+		::std::string propertyFilePathStr(argv[2]);
+		result = doArchive(propertyFilePathStr);
+	} else if (command.compare("expand") == 0) {
+		// WAFファイルの展開
+		::std::string wafFilePathStr(argv[2]);
+		result = doExpand(wafFilePathStr);
+	} else {
+		// エラー
+		result = -1;
+	}
+	LOG4CXX_DEBUG(log, "end.");
+	return result;
 } catch (::std::exception &e ) {
 	::std::cout << e.what() << ::std::endl;
 }
